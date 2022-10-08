ef4bdb1dde905aa720296e72cd1496544153e372
allan.lima <allanpinheirodelima@gmail.com>
2022-10-08 04:33:53 -0300

    Add an explanation to the Pinia line

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,11 @@ import {pinia} from "@/store";
 const app = createApp(App);
 
 app.use(router)
+
+/** Pinia is a new store management solution from Vue Team
+ *  it is meant to be a substitute of Vuex
+ *  You can find more information at https://pinia.vuejs.org/introduction.html
+ * */
 app.use(pinia)
 
 app.mount('#app')
