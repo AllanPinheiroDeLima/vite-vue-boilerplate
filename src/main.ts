445a1137f92e8c7dec33bf1e1f9c00bc069ad158
allan.lima <allanpinheirodelima@gmail.com>
2022-10-08 04:30:22 -0300

    Add pinia to initial boilerplate

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import {router} from "@/router";
+import {pinia} from "@/store";
 
 const app = createApp(App);
 
 app.use(router)
+app.use(pinia)
 
 app.mount('#app')
